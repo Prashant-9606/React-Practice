0bf1e2a807272bac4191fde0d5d39c65ce483559
Prashant-9606 <prashant060200@gmail.com>
2023-05-30 20:18:57 +0530

    addded about component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import './App.css';
+// import About from './component/About';
 import Navbar from './component/Navbar';
-import Textform from './component/Textform';
+ import Textform from './component/Textform';
 
 function App() {
   return (
     <>
     <Navbar title="Text Manager" aboutText="About Us" searchBarText="Enter to Search"/>
     <div className="container my-3">
-      <Textform />
+     <Textform />
+      {/* <About/> */}
     </div>
     </>
   );
--- src/component/Textform.js
+++ src/component/Textform.js
@@ -4,12 +4,20 @@ export default function Textform() {
 
     const[text,setText] =useState("Enter text here");
 
-    const handleClick =()=> {
+    const handleUpClick =()=> {
         console.log("button Clicked");
+        let newText = text.toUpperCase();
+        setText(newText);
     }
 
-    const handleOnChange =() =>{
+    const handleOnChange =(e) =>{
         console.log("On Change");
+        setText(e.target.value)
+    }
+
+    const handleLowClick =() => {
+        let newText=text.toLowerCase();
+        setText(newText);
     }
 
     return (
@@ -17,7 +25,13 @@ export default function Textform() {
     <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="Manage Text" rows="8"></textarea>
     </div>
-    <button className="btn btn-primary" onClick={handleClick}>Convert to Uppercase</button>
+    <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+    <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
+    <div className="container my-3">
+        <h1>Your Text Summary</h1>
+        <p>{text.split(" ").length} words, {text.length} characters</p>
+        <p>{text.split(" ").length * 0.008} Minutes to Read</p>
+    </div>
     </>
   )
 }
